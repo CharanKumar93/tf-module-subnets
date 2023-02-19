bd017189fbdc78f30a5a5e3616f4ab3099eafdd5
Charan <gcharankumar99@gmail.com>
2023-02-19 15:33:32 -0600

    vars file

--- subnets/vars.tf
+++ subnets/vars.tf
@@ -5,11 +5,3 @@ variable "env" {}
 variable "default_vpc_id" {}
 variable "name" {}
 variable "vpc_peering_connection_id" {}
-
-variable "cidr_block" {}
-variable "availability_zone" {}
-variable "vpc_id" {}
-variable "env" {}
-variable "default_vpc_id" {}
-variable "name" {}
-variable "vpc_peering_connection_id" {}
